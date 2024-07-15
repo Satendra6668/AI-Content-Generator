fb1aae6718d4e65ff823d95ee50c7f32e30a6a56
Satendra <satendra6668@gmail.com>
2024-07-15 07:57:06 +0530

    first commit

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./utils/schema.tsx",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url:'postgresql://accounts:nVjAHW1EL6qb@ep-curly-queen-a5s6p09k.us-east-2.aws.neon.tech/AI-Content-Generator?sslmode=require',
+    }
+  };
+  
